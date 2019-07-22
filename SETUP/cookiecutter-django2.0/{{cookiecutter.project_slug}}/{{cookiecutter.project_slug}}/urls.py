d5e2786f75696a749e6fc6048b7449c8ad44be8f
jstrickler <jstrickler@gmail.com>
2019-07-22 12:13:12 -0400

    updated project urls in cookiecutter-django2.0 template

--- SETUP/cookiecutter-django2.0/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ SETUP/cookiecutter-django2.0/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -13,7 +13,7 @@ Class-based views:
 
 Including another (usually an app's) URLconf:
     1. Import the include() function: from django.urls import url, include
-    2. Add a URL to urlpatterns:  path('appname', include('app.urls', namespace="app"))
+    2. Add a URL to urlpatterns:  path('appname', include('app.urls'))
     Path may be blank; in that case only the app URL is used
 """
 from django.conf import settings
@@ -23,10 +23,23 @@ from django.contrib import admin
 # site-wide route mapping
 urlpatterns = [
     path('admin', admin.site.urls),
-#    path('', include(('app.urls', "app"))),  # delegate to app's URL config
-    # note: if path (1st arg) is empty, it will be a prefix to app URL
-    # ie, if path here is "spam" and path in app/urls.py is "home",
+    # To include urls from an app:
+    #
+    # path('', include(('app.urls'))),  # delegate to app's URL config
+    #
+    # note: if path (1st arg) is not empty, it will be a prefix to app URL
+    # and will follow the hostname, before the path in the app's urls.py
+    #
+    # i.e., if path here is "spam" and path in app/urls.py is "home",
     # url will be HOSTNAME:PORT/spam/home
+    #
+    # if first arg is empty, then paths in app.urls will immediately follow
+    #  the host name
+
+    # To configure a single view:
+    #
+    # from app.views import myview
+    # path('viewpath', myview, name="myview")
 ]
 
 # include Django Debug toolbar if DEBUG is set
